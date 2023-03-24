3e675bd1f4475045849e76b01bee45bc2d7e9af9
Suhas Katrahalli <suhaskk2002@gmail.com>
2023-03-24 23:10:22 +0530

    Add files via upload
    Code to display live camera feed from mobile and capture it

---
+++ streamvideo.py
@@ -0,0 +1,26 @@
+import streamlit as st
+import cv2
+import numpy as np
+import requests
+
+# Title of the app
+st.title( 'Mobile Camera Preview ' )
+
+# An empty image container that will hold the frames we'll fetch from the server
+frame_window = st.image( [] )
+
+take_picture_button = st.button( 'Take Picture' )
+while True:
+    # Request the image from the server
+    response = requests.get(url="http://192.168.1.7:8080//photo.jpg")
+    imgNp = np.array(bytearray(response.content), dtype=np.uint8)
+    frame = cv2.imdecode(imgNp, cv2.IMREAD_UNCHANGED ) 
+    # As OpenCV decodes images in BGR format, we'd convert it to the RGB format
+    frame = cv2.cvtColor( frame , cv2.COLOR_BGR2RGB )
+
+    frame_window.image(frame)
+
+    if take_picture_button:
+        # Pass the frame to a model
+        # And show the output here...
+        break
